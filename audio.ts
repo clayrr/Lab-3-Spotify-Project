9e76aed70450f726d401010bba5006541e12a6c9
Claire Tsai <claire_tsai@horacemann.org>
2025-01-08 22:03:34 -0500

    code

---
+++ audio.ts
@@ -0,0 +1,49 @@
+//-----------------------------
+//#region Database Connection
+//-----------------------------
+import path from "path";
+import {Request, Response} from 'express';
+import sqlite3 from "sqlite3";
+import { RequestHandler } from "express-serve-static-core";
+import { ParsedQs } from "qs";
+const sqlite = sqlite3.verbose();
+const dbFile = path.join(__dirname, "foo.db");
+// below is the line for vanilla ES6 js to work; not necessary with typescript
+// const dbFile = path.join(path.dirname(fileURLToPath(import.meta.url)), "foo.db");
+const db = new sqlite.Database(dbFile, (error) => {
+  if (error) return console.error(error.message);
+  console.log(`Connected to database ${dbFile}`);
+});
+
+//get spotify API stuff - code from the website - ...?  - it doesn't work :(
+/*curl --request GET \
+  --url https://api.spotify.com/v1/audio-features/11dFghVXANMlKmJXsNCbNl \
+  --header 'Authorization: Bearer 1POdFZRZbvb...qqillRxMr2z'
+*/
+//need: client id, spotify track ID
+
+//map track 
+
+function matchEnergyLevel(energy: number): string {
+    if (energy < 0.25) {
+      return 'Low energy)';
+    } else if (energy < 0.5) {
+      return 'Moderately low energy)';
+    } else if (energy < 0.75) {
+      return 'Moderate energy)';
+    } else {
+      return 'High energy)';
+    } 
+  }
+  
+  // endpoint
+  /*
+  app.get('/track-energy', (req: Request, res: Response) => {
+      const energyLevel = matchEnergyLevel(energy);
+  
+      res.json({
+          message: `Based on your top music taste, we have concluded that you have ${energyLevel}.`
+      });
+  });
+  */
+
