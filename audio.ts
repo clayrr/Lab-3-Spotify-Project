e51acbc74ecc8023bdb84a2374056898ba58f0f0
Maya Rangarajan <maya_rangarajan@horacemann.orgm>
2025-01-11 19:18:57 -0500

    check of all three + fix audio + create seasons

--- audio.ts
+++ audio.ts
@@ -1,8 +1,14 @@
 //-----------------------------
 //#region Database Connection
 //-----------------------------
+import express, { Request, Response } from 'express';
+
+
+const app = express();
+const port = 3000;
+
+/*
 import path from "path";
-import {Request, Response} from 'express';
 import sqlite3 from "sqlite3";
 import { RequestHandler } from "express-serve-static-core";
 import { ParsedQs } from "qs";
@@ -24,26 +30,32 @@ const db = new sqlite.Database(dbFile, (error) => {
 
 //map track 
 
+const energy = 0.5;
+
 function matchEnergyLevel(energy: number): string {
-    if (energy < 0.25) {
-      return 'Low energy)';
-    } else if (energy < 0.5) {
-      return 'Moderately low energy)';
-    } else if (energy < 0.75) {
-      return 'Moderate energy)';
-    } else {
-      return 'High energy)';
-    } 
+  if (energy < 0.25) {
+    return 'Low energy)';
+  } else if (energy < 0.5) {
+    return 'Moderately low energy)';
+  } else if (energy < 0.75) {
+    return 'Moderate energy)';
+  } else {
+    return 'High energy)';
   }
-  
-  // endpoint
-  /*
-  app.get('/track-energy', (req: Request, res: Response) => {
-      const energyLevel = matchEnergyLevel(energy);
-  
-      res.json({
-          message: `Based on your music taste, we have concluded that you have ${energyLevel}.`
-      });
+}
+
+// endpoint
+
+app.get('/track-energy', (req: Request, res: Response) => {
+  const energyLevel = matchEnergyLevel(energy);
+
+  res.json({
+    message: `Based on your music taste, we have concluded that you have ${energyLevel}.`
   });
-  */
+});
+
+// start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
 
