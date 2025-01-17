e12a3fd03fb4cd858de7347db5cf86d4495c4f2b
Claire Tsai <claire_tsai@horacemann.org>
2025-01-17 14:17:38 -0500

    chagne localhost number

--- seasons.ts
+++ seasons.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from 'express';
 
 const app = express();
-const port = 3000;
+const port = 8888;
 
 // simulated value for demonstration
 const danceability = 0.79;
