e87dd9b7d2d5596a32f722b4e9915af69157a701
Claire Tsai <claire_tsai@horacemann.org>
2025-05-27 23:19:57 -0400

    chart stuff

--- spotif/src/main.js
+++ spotif/src/main.js
@@ -2,3 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+ 
+import CanvasJSChart from '@canvasjs/vue-charts';
+const app = createApp(App);
+app.use(CanvasJSChart); // install the CanvasJS Vuejs 
+app.mount('#app');     
